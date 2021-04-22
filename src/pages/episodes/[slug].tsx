caa0f02042de4533aeba17a50e655ffb65814ec4
GuilhermeProenca <cravo-guilherme1@hotmail.com>
2021-04-22 16:04:07 -0300

    NLW5-Dia4

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -2,6 +2,7 @@ import { GetStaticPaths, GetStaticProps } from 'next';
 import{ format, parseISO } from 'date-fns';
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utilis/convertDurationToTimeString';
+import { useRouter } from 'next/router';
 
 import Image from 'next/image';
 import Link from 'next/link';
@@ -27,7 +28,6 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
-  
     return (
         <div className={styles.episode}>
             <div className={styles.thumbnailContainer}>
@@ -60,8 +60,24 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+    const { data } = await api.get('episodes', {
+        params: {
+            _limit: 2,
+            _sort: 'published_at',
+            _order: 'desc'
+        }
+    })
+
+    const paths = data.map(episode => {
+        return {
+            params: {
+                slug: episode.id
+            }
+        }
+    })
+    
     return {
-        paths: [],
+        paths,
         fallback: 'blocking'
     }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import { GetStaticProps } from 'next';
 import{ format, parseISO } from 'date-fns';
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utilis/convertDurationToTimeString';
+import { useContext } from 'react';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 import Image from 'next/image';
 import Link from 'next/link';
@@ -27,6 +29,8 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes ,allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext);
+  
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -53,7 +57,7 @@ export default function Home({ latestEpisodes ,allEpisodes }: HomeProps) {
                     <span>{episode.durationAsString}</span>
                   </div>
 
-                  <button type="button">
+                  <button type="button" onClick={() => play(episode)}>
                     <img src="/play-green.svg" alt="Tocar episódio"/>
                   </button>
                 </li>
