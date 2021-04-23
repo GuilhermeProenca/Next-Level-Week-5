21b750745872569b57cd3cb9667f5301fab0457c
GuilhermeProenca <cravo-guilherme1@hotmail.com>
2021-04-23 19:11:08 -0300

    NLW5-Dia5

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -79,9 +79,8 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
     function playNext() {
         if (isShuffling) {
             const nextRandomEpisodeIndex = Math.floor(Math.random() * episodeList.length);
-            setCurrentEpisodeIndex(nextRandomEpisodeIndex);
-        }
-        else if (hasNext) {
+            setCurrentEpisodeIndex(nextRandomEpisodeIndex);          
+        } else if (hasNext) {
             setCurrentEpisodeIndex(currentEpisodeIndex + 1);
         }
     }
